3434aa8cf633041f8aef72af0df326e720d6efad
Meeria <meria.dap@gmail.com>
2022-11-21 22:40:32 +0100

    maj

--- portfolio2022/contact.php
+++ portfolio2022/contact.php
@@ -1,6 +1,30 @@
-<?php
 
-$to = "meriadec.denier@sts-sio-caen.info"
+<?php 
 
-mail("meriadec.denier@sts-sio-caen.info","sujet", "message", "")
+// $to      = 'meria.dap@hotmail.fr';
+// $subject = 'le sujet';
+// $message = 'Bonjour !';
+// $headers = 'From: meria.dap@gmail.com' . "\r\n" ;
 
+
+// if(mail($to, $subject, $message, $headers))
+// echo 'envoyé !';
+// else
+// echo 'erreur';
+
+
+
+if(isset($POST['mailform']))
+
+if (!empty($post['mail']) and !empty($post['nom']) and !empty($post['message'])){
+
+}
+else{
+    $msg='tous les champs doivent être remplis';
+}
+
+
+
+			if (isset($msg)){
+				echo $msg;
+			}
